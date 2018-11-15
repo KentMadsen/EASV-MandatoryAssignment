15460a94952f4cc1804d266588160ed99d9759cf
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-11-15 12:35:42 +0100

    p

--- web/dashboard.js
+++ web/dashboard.js
@@ -1,7 +1,6 @@
 // Mandatory Assignment
 // Dashboard
- // Class Definitions
- //
+  // Class Definitions
 class Vector
 {
     //
@@ -11,11 +10,32 @@ class Vector
         this.y = vY;
     }
 
+//
+    getX()
+    {
+      return this.x;
+    }
+
+    setX( value )
+    {
+      this.x = value;
+    }
+
+//
+    getY()
+    {
+      return this.y;
+    }
+
+    setY( value )
+    {
+      this.y = value;
+    }
 
     // Works
     substract( px, py )
     {
-        var newVector = GenVec( ( this.x - px ), 
+        var newVector = GenerateVector( ( this.x - px ),
                                 ( this.y - py ) );
         return newVector;
     }
@@ -23,22 +43,51 @@ class Vector
     // Works
     addition( px, py )
     {
-        var newVector = GenVec( ( this.x + px ), 
+        var newVector = GenerateVector( ( this.x + px ),
                                 ( this.y + py ) );
         return newVector;
     }
 
     // Works
-    scale( s )
+    scaleByScalar( s )
     {
-        var newVector = GenVec( ( s * this.x ),
+        var newVector = GenerateVector( ( s * this.x ),
                                 ( s * this.y ) );
         return newVector;
     }
 
+    // Not sure ?
+    unitVector()
+    {
+      // x_e = (x)/|v| and y_e = (y)/|v|
+      var newVector = GenerateVector( ( this.x / this.distance() ),
+                              ( this.y / this.distance() ) );
+      return newVector;
+    }
+
+    // NotSure
+    /*scalarVector(v)
+    {
+
+          var newVector = GenVec( ( this.x + px ),
+                                  ( this.y + py ) );
+          return newVector;
+    }
+    */
+
+    //
+    projectAgaintsUnitVector( v )
+    {
+      var newVector = GenerateVector( ( v.x * this.distance() ),
+                              ( v.y * this.distance() ) );
+
+      return newVector;
+    }
+
+    // Works
     pow2( value )
     {
-        return Math.pow( value, 
+        return Math.pow( value,
                          two );
     }
 
@@ -47,101 +96,385 @@ class Vector
     distance()
     {
         // x^2
-        var x_result = this.pow2(this.x);
-                                     
+        var x_result = this.pow2( this.x );
+
         // y^2
-        var y_result = this.pow2(this.y);
+        var y_result = this.pow2( this.y );
 
         // x_result + y_result
         var combined = (x_result + y_result);
-        
+
         // sqrt(x^2 + y^2) = |v^(->)|
         return Math.sqrt( combined );
     }
 }
 
-    // Generators
-function GenVec( pX, pY )
+// Generators
+  // Set Parameters
+function GenerateVector( pX, pY )
 {
-    return new Vector( pX, pY );
+  return new Vector( pX, pY );
 }
-    
+
+  // Default Generic Object
 function GenEmptyVector()
 {
-    return GenVec(0, 0);
+  return GenerateVector( 0, 0 );
 }
-
-
-/* 
-*/
-class Entity
-{
-    constructor();
-
-};
-
-/* 
- *
- */
-class Actor 
-    extends Entity
-{
-    constructor();
-
-};
-
-/*
- * 
- */
-class Effect 
-    extends Entity
-{
-    constructor();
-
-};
-
-/* 
- *
- */
-class Interactive 
-    extends Entity
-{
-    constructor();
-
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/* 
- *
- */
-class CameraField 
-    extends Actor
-{
-    constructor();
-
-};
-
 
 // Vector Coordinate
 class Point
 {
+  //
     constructor( pX, pY )
     {
         this.x = pX;
         this.y = pY;
     }
 
+    //
+    getX()
+    {
+      return this.x;
+    }
+
+    setX( value )
+    {
+      this.x = value;
+    }
+
+    //
+    getY()
+    {
+      return this.y;
+    }
+
+    setY( value )
+    {
+      this.y = value;
+    }
+
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // Choose parameters
+function GeneratePoint( pX, pY )
+{
+  return new Point( pX, pY );
+}
+
+  // Default Generic Object
+function GenerateEmptyPoint()
+{
+  return GeneratePoint( 0, 0 );
+}
+
+/*
+*/
+class Entity
+{
+    constructor()
+    {
+      this.originPoint = GenEmptyVector();
+
+    };
+
+
+
+};
+
+/*
+ *
+ */
+class Actor
+    extends Entity
+{
+    constructor()
+    {
+      super();
+
+    };
+
+};
+
+class Object
+  extends Entity
+{
+  constructor()
+  {
+    super();
+
+  };
+
+
+};
+
+/*
+ *
+ */
+class Effect
+    extends Entity
+{
+    constructor()
+    {
+      super();
+      //
+    };
+
+
+
+};
+
+function toRadians( degrees )
+{
+    return degrees * Math.PI/180;
+}
+
+function toDegress( radians )
+{
+  return radians * 180/Math.PI;
+}
+
+
+class VectorParameterFunction
+{
+  constructor()
+  {
+    this.pos = GenerateEmptyPoint();
+  }
+
+  apply_x( i )
+  {
+    return -1;
+  }
+
+  apply_y( i )
+  {
+    return -1;
+  }
+
+}
+
+// Tag: Circle class
+class Circle 
+  extends VectorParameterFunction
+{
+  constructor()
+  {
+    super();
+
+    this.radius = 1; 
+  }
+
+  getRadius()
+  {
+    return this.radius;
+  }
+
+  setRadius( i )
+  {
+    this.radius = i;
+  }
+
+}
+
+Circle.prototype.apply_x = 
+  function( i )
+{
+  return this.pos.getX() + this.r * Math.cos( toRadians( i ) );
+}
+
+Circle.prototype.apply_y = 
+  function( i )
+{
+  return this.pos.getY() + this.r * Math.sin( toRadians( i ) );
+}
+
+class Counter
+{
+  constructor()
+  {
+    this.value = 0;
+
+    this.default_distance = 1;
+  }
+
+  reset()
+  {
+    this.setValue(0);
+  }
+
+  increase( i )
+  {
+    this.setValue( this.getValue() + i )
+  }
+
+  increment()
+  {
+    this.increase( this.default_distance );
+  }
+
+  decrease( i )
+  {
+    this.setValue( this.getValue() - i )
+  }
+
+  decrement()
+  {
+    this.decrease( this.default_distance );
+  }
+
+  setValue( i )
+  {
+    this.value = i;
+  }
+
+  getValue()
+  {
+    return this.value;
+  }
+  
+}
+
+class particle 
+  extends Point
+{
+  constructor()
+  {
+      super();
+
+      this.dv = GenerateEmptyPoint();
+  }
+
+}
+
+//
+class ParticleSystem 
+  extends Effect
+{
+  //
+  constructor()
+  {
+      super();
+
+      this.number_of_particles = 256;
+      this.particles           = new Array();
+      
+      this.sequence   = new Counter();
+  };
+
+  setOrigin( x, y )
+  {
+    this.originPoint = GeneratePoint(x, y);
+  }
+
+  // amount of given entries, over a specific distance
+  apply( n_entries, distance )
+  {
+    var du = (distance / n_entries);
+    return du;
+  }
+
+  append( particle_element )
+  {
+    this.particles = this.particles.concat( particle_element );
+  }
+
+  //
+  generate()
+  {
+    var ps = this.number_of_particles;
+
+    //
+    for( var i = 0; 
+             i < ps; 
+             i ++ )
+    {
+      
+      this.append();
+      
+
+    }
+    
+  };
+
+  //
+  debug()
+  {
+    for( var i = 0; 
+             i < this.particles.length; 
+             i++)
+    {
+      //
+      var current = this.particles[i];
+
+      var actual_position = GeneratePoint( this.originPoint.getX() + current.x, 
+                                           this.originPoint.getY() + current.y );
+
+      illustrate_point( actual_position, 
+                        GenerateVector(8, 1), 
+                        'red', 
+                        'white' );
+    }
+  };
+
+  // next 'Frame'
+  update()
+  {
+    Counter.increase(1);
+    
+
+  };
+
+};
+
+/*
+ *
+ */
+class Interactive
+    extends Entity
+{
+    constructor()
+    {
+      super();
+      //
+    };
+
+};
+
+//
+class Model
+{
+  constructor()
+  {
+    this.originPoint = GenEmptyVector();
+
+
+  };
+
+}
+
+class Container
+{
+  constructor()
+  {
+
+  };
+}
+
+/*
+ *
+ */
+class CameraField
+    extends Actor
+{
+    constructor()
+    {
+      super();
+      //
+
+    };
+
+};
 
 // Tag: Variables
     // Application Variables
@@ -154,84 +487,91 @@ var Continue = true;
 var start_screen_pos = new Point(0, 0);
 
 // Properties
-var canvas_size = new Vector(1280, 720);
+var canvas_size = new Vector( 1280, 720 );
 var canvas_id_name = 'dashboard';
 
 // Static Globals
 const two = 2;
 const dimension = '2d';
 
+const debug_system = true;
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
+var cameras = new CameraField(1);
+var camera_index = 0;
+
+//
 
 // Tag: context functions
 // Draw on Context: Base Functions
 // Base Drawing
-function line( point_begin, 
+function line( point_begin,
                point_end )
-{    
+{
     context.beginPath();
 
-    context.moveTo( point_begin.x, 
-                    point_begin.y );
+    context.moveTo( point_begin.x,
+                    inverse_y_axis_along_canvas(point_begin.y) );
 
-    context.lineTo( point_end.x, 
-                    point_end.y );
+    context.lineTo( point_end.x,
+                    inverse_y_axis_along_canvas(point_end.y) );
 
     context.stroke();
 }
 
+// P: p.x, p.y, center point, inside x, y coordinate system
+// V: v.x = radius, v.y = width
+//
+function illustrate_point( p, 
+                           v, 
+                           fillcolor, 
+                           strokecolor )
+{
+  // Skip, if debugging is off
+  if( debug_system == false )
+    return;
+  //
+  context.beginPath();
+
+  context.arc( p.x, inverse_y_axis_along_canvas( p.y ),
+               v.x,
+               0, (2 * Math.PI) );
+
+  context.fillStyle = fillcolor;
+  context.fill();
+
+  context.lineWidth = v.y;
+  context.strokeStyle = strokecolor;
+
+// Draw
+  context.stroke();
+}
+
+///////////////////////////////////////////////////////
+function inverse_y_axis_along_canvas( y_position )
+{
+  // canvas_height - y = ( y+ )
+  var rv = canvas_size.y -  y_position;
+
+// Inverses the y position on the canvas, so it's drawn alon the positive side of y, than -
+  return rv;
+}
+
 // Parameters: SP (Start Point), V: Vector (size and direction)
-function clearScreenAt(SP, V)
+function clearScreenAt( SP, V )
 {
     var toPoint = V.addition( SP.x, SP.y );
 
-    context.clearRect( SP.x, SP.y, 
+    context.clearRect( SP.x, SP.y,
                        toPoint.x, toPoint.y);
 }
 
 function clearScreen()
 {
     clearScreenAt( start_screen_pos,
-                   canvas_size )   
+                   canvas_size )
 }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Program Logic
-    // Initialise Stage
-function init()
-{
-    //
-    configure();
-
-    //
-    prepare();
-
-    // Starts system
-    next();
-}
-
         // Configuration
 function configure()
 {
@@ -247,41 +587,9 @@ function configure_stage()
     context = canvas_id.getContext( dimension );
 }
 
-        //
-function prepare()
-{
-
-}
-
-// Draw Operations
-function draw()
-{
-    clearScreen();
-
-    //
-    analyze();
-
-    // 
-    map();
-
-    // Outline
-    representation();
-
-    // Draw
-    present();
-
-    next();
-}
-
 // Phrases
-    //
-function clear()
-{
-    // Clean current Canvas
-    clearScreen();
-}
 
-    // 
+    //
 function analyze()
 {
 
@@ -293,24 +601,87 @@ function map()
 
 }
 
-    // 
+    //
 function representation()
 {
 
 }
 
-    // final stage: 
+    // final stage:
 function present()
 {
 
 }
 
-// Internal Business Logic Phrase
-function update()
-{
+// Program Structure
 
-    
-    next();
+// Internal Business Logic Phrase
+//
+function prepare()
+{
+  prepare_test();
+}
+
+
+var ps_test;
+
+function prepare_test()
+{
+  ps_test = new ParticleSystem();
+  ps_test.generate();
+  
+
+  var c = new Circle();
+
+  console.log(c);
+
+};
+
+// Initialise Stage
+function init()
+{
+  var c = new Counter();
+
+
+  console.log(c);
+
+  //
+  configure();
+
+  //
+  prepare();
+
+  // Starts system
+  next();
+}
+
+
+// Draw Operations
+function draw()
+{
+  clear();
+
+  //
+  analyze();
+
+  //
+  map();
+
+  // Outline
+  representation();
+
+  // Draw
+  present();
+
+  //
+  next();
+}
+
+// inside of next, maybe ?
+function clear()
+{
+  // Clean current Canvas
+  clearScreen();
 }
 
 
@@ -325,14 +696,24 @@ function next()
 
         // Update System Model
         update();
-        
+
         //
         window.requestAnimationFrame( draw );
     }
 }
 
+//
 function additional()
 {
+
+}
+
+//
+
+function update()
+{
+  ps_test.update();
+
 
 }
 
