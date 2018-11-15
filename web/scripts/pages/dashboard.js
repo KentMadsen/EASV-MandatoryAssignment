d2af3d3c7e7da740f4484a22884669bdaa0472d3
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-11-15 21:39:08 +0100

    restructuring scripts

--- web/scripts/pages/dashboard.js
+++ web/scripts/pages/dashboard.js
@@ -6,21 +6,8 @@
 */
 
 // Tag: Global Static Variables
-  const zero  = 0;
-  const one   = 1;
-  const two   = 2;
-  const three = 3;
-  const four  = 4;
-  const five  = 5;
-  const six   = 6;
-  const seven = 7;
-  const eight = 8;
-  const nine  = 9;
-  
   const d2 = '2d';
   const d3 = '3d';
-  
-  const debug_system = true;
 
 // Tag: Basic calculation functions
 function basicSubstract( a, b )
--- web/scripts/property/defaults.js
+++ web/scripts/property/defaults.js
@@ -5,4 +5,15 @@
     Copyright 2018 Kent v Madsen
 */
 const default_wait = 25;
+
+const zero  = 0;
+const one   = 1;
+const two   = 2;
+const three = 3;
+const four  = 4;
+const five  = 5;
+const six   = 6;
+const seven = 7;
+const eight = 8;
+const nine  = 9;
   
