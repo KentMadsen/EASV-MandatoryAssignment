c5a6dcee2fa90e20e1f46aadbe04c4313baaf5ce
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-11-16 16:14:12 +0100

    a

--- FrontEnd/WebApplication/Scripts/Application.js
+++ FrontEnd/WebApplication/Scripts/Application.js
@@ -1,14 +1,37 @@
-class Frame
+class ApplicationFramework
 {
     constructor()
     {
-        this._name = "Valkyrier";
-        this._description = "";
-        this._
+        this.name = "Valkyrier";
+        this.description = "";
+
+        this.language = "en-GB";
+        
+        this.current_view = 0;
     }
+
+    // Setup Methods
+    setup()
+    {
+
+    }
+
+
+
+
+    
 }
 
+var application = null;
+
+// Application start
 function entry()
 {
+    application = new ApplicationFramework();
+    
+    application.setup();
 
+    console.log(application);
 }
+
+entry();
--- FrontEnd/WebApplication/Scripts/Requirement.js
+++ FrontEnd/WebApplication/Scripts/Requirement.js
@@ -1,16 +1,24 @@
 //
 function requirement( package, 
-                      type, 
-                      name )
+                      module, 
+                      class_name )
 {
     
 
 }
 
-function insertScript( link_to_source )
+// Appends to the current html page, a required script
+function appendScript( link_to_source )
 {
-    var script = document.createElement("script");
+    var script = document.createElement( "script" );
+ 
     script.type = script_element_type;
-    script.src = link_to_source;
+    script.src  = link_to_source;
+
     document.head.append();
 }
+
+function removeScript()
+{
+
+};
