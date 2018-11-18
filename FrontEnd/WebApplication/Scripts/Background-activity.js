9f95644f178ee502500dfcc01893d49dd2a7030f
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-11-18 03:28:55 +0100

    done with header

---
+++ FrontEnd/WebApplication/Scripts/Background-activity.js
@@ -0,0 +1,28 @@
+var id = document.getElementById('background-gratident-fonts');
+var id = id.getContext();
+
+function initialise()
+{
+
+
+    next;
+}
+
+function move()
+{
+
+}
+
+function next()
+{
+    move();
+
+    for( var i = 0; 
+         i < elements.length; 
+         i++ )
+    {
+        e = elements[i];
+    }
+
+    window.setTimeout( next, 200 );
+}
