768b033fd1136afbc3293c09da120044150b0d7f
Kent Madsen <20760795+KentMadsen@users.noreply.github.com>
2018-11-30 16:16:00 +0100

    a

---
+++ backend/library/src/Application.java
@@ -0,0 +1,26 @@
+public class Application
+{
+    public static void main( String[] args )
+    {
+        Application application = new Application();
+
+        application.initialise();
+        application.execute();
+        application.deinitialise();
+    }
+
+    public void initialise()
+    {
+
+    }
+
+    public void execute()
+    {
+
+    }
+
+    public void deinitialise()
+    {
+
+    }
+}
